92a888356c512220fd5e44b24634eabdf4904117
ajardeen <mnazar87540@gmail.com>
2024-08-15 20:37:21 +0530

    first login Security added

---
+++ src/main/java/com/springsec/springsecproject/SpringsecprojectApplication.java
@@ -0,0 +1,14 @@
+package com.springsec.springsecproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecprojectApplication.class, args);
+		System.out.println("welcome");
+	}
+
+}
---
+++ src/test/java/com/springsec/springsecproject/SpringsecprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsec.springsecproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
